6c5e436b94d7054dec6e0a8a2a3cb843079191e0
Bram Van der Sype <bvandersype@moovly.com>
2013-11-26 10:23:27 +0100

    -n MOAR TESTS

--- Tests/EventListener/ViewListenerTest.php
+++ Tests/EventListener/ViewListenerTest.php
@@ -10,22 +10,19 @@ class ViewListenerTest extends \PHPUnit_Framework_TestCase
     /** @var ViewListener */
     private $listener;
 
-    protected function setUp()
+    public function testonControllerResponse()
     {
         $templating = $this->getMockBuilder('\Symfony\Bundle\TwigBundle\TwigEngine')
             ->disableOriginalConstructor()
             ->getMock();
 
-        $templating->expects($this->any())
+        $templating->expects($this->once())
             ->method('renderResponse')
             ->with($this->equalTo('AcmeDemoBundle:Foo:bar.html.twig'), $this->equalTo(['foo' => 'bar']))
             ->will($this->returnValue(new Response()));
 
-        $this->listener = new ViewListener($templating);
-    }
+        $listener = new ViewListener($templating);
 
-    public function testonControllerResponse()
-    {
         // mocking away
         $paramBag = $this->getMockBuilder('\Symfony\Component\HttpFoundation\ParameterBag')
                 ->getMock();
@@ -33,26 +30,6 @@ class ViewListenerTest extends \PHPUnit_Framework_TestCase
             ->method('get')
             ->will($this->returnValue('acme_demo.controller.foo:barAction'));
 
-        $this->doTest($paramBag);
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     */
-    public function testIncorrectController()
-    {
-        // mocking away
-        $paramBag = $this->getMockBuilder('\Symfony\Component\HttpFoundation\ParameterBag')
-                ->getMock();
-        $paramBag->expects($this->once())
-            ->method('get')
-            ->will($this->returnValue('AcmeDemoBundle:Foo:bar'));
-
-        $this->doTest($paramBag);
-    }
-
-    protected function doTest($paramBag)
-    {
         $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')
             ->enableArgumentCloning()
             ->getMock();
@@ -61,11 +38,46 @@ class ViewListenerTest extends \PHPUnit_Framework_TestCase
         $event = $this->getMockBuilder('\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')
             ->disableOriginalConstructor()
             ->getMock();
-        $event->expects($this->any())
+        $event->expects($this->once())
             ->method('getControllerResult')
             ->will($this->returnValue(['foo' => 'bar'])); // the foo from the templating mock
 
-        $event->expects($this->any())
+        $event->expects($this->once())
+            ->method('getRequest')
+            ->will($this->returnValue($request));
+
+        $listener->onControllerResponse($event);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testIncorrectController()
+    {
+
+        $templating = $this->getMockBuilder('\Symfony\Bundle\TwigBundle\TwigEngine')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->listener = new ViewListener($templating);
+
+        // mocking away
+        $paramBag = $this->getMockBuilder('\Symfony\Component\HttpFoundation\ParameterBag')
+                ->getMock();
+        $paramBag->expects($this->once())
+            ->method('get')
+            ->will($this->returnValue('AcmeDemoBundle:Foo:bar'));
+
+        $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')
+            ->enableArgumentCloning()
+            ->getMock();
+        $request->attributes = $paramBag;
+
+        $event = $this->getMockBuilder('\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $event->expects($this->once())
             ->method('getRequest')
             ->will($this->returnValue($request));
 
