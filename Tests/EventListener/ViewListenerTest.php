5e82edc744fe6ace6d405f05e0aa300aefefeb1e
Bram Van der Sype <bvandersype@moovly.com>
2014-02-11 14:40:41 +0100

    -n cleaned up test

--- Tests/EventListener/ViewListenerTest.php
+++ Tests/EventListener/ViewListenerTest.php
@@ -3,50 +3,48 @@
 namespace Brammm\CommonBundle\Tests\EventListener;
 
 use Brammm\CommonBundle\EventListener\ViewListener;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ViewListenerTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var ViewListener */
-    private $listener;
+    /** @var \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent */
+    private $eventMock;
+    /** @var \PHPUnit_Framework_MockObject_MockObject|\Symfony\Bundle\TwigBundle\TwigEngine */
+    private $twigMock;
 
-    public function testonControllerResponse()
+    public function setUp()
     {
-        $templating = $this->getMockBuilder('\Symfony\Bundle\TwigBundle\TwigEngine')
+        $this->twigMock = $this->getMockBuilder('\Symfony\Bundle\TwigBundle\TwigEngine')
             ->disableOriginalConstructor()
             ->getMock();
 
-        $templating->expects($this->once())
+        $this->eventMock = $this->getMockBuilder(
+            '\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent'
+        )
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testonControllerResponse()
+    {
+        $request = $this->getRequestMock('acme_demo.controller.foo:barAction');
+
+        $this->eventMock->expects($this->once())
+            ->method('getRequest')
+            ->will($this->returnValue($request));
+
+        $this->eventMock->expects($this->once())
+            ->method('getControllerResult')
+            ->will($this->returnValue(['foo' => 'bar'])); // the foo from the templating mock
+
+        $this->twigMock->expects($this->once())
             ->method('renderResponse')
             ->with($this->equalTo(':Demo:Foo/bar.html.twig'), $this->equalTo(['foo' => 'bar']))
             ->will($this->returnValue(new Response()));
 
-        $listener = new ViewListener($templating);
-
-        // mocking away
-        $paramBag = $this->getMockBuilder('\Symfony\Component\HttpFoundation\ParameterBag')
-                ->getMock();
-        $paramBag->expects($this->once())
-            ->method('get')
-            ->will($this->returnValue('acme_demo.controller.foo:barAction'));
-
-        $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')
-            ->enableArgumentCloning()
-            ->getMock();
-        $request->attributes = $paramBag;
-
-        $event = $this->getMockBuilder('\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $event->expects($this->once())
-            ->method('getControllerResult')
-            ->will($this->returnValue(['foo' => 'bar'])); // the foo from the templating mock
-
-        $event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($request));
-
-        $listener->onControllerResponse($event);
+        $listener = new ViewListener($this->twigMock);
+        $listener->onControllerResponse($this->eventMock);
     }
 
     /**
@@ -54,33 +52,34 @@ class ViewListenerTest extends \PHPUnit_Framework_TestCase
      */
     public function testIncorrectController()
     {
+        $request = $this->getRequestMock('AcmeDemoBundle:Foo:bar');
 
-        $templating = $this->getMockBuilder('\Symfony\Bundle\TwigBundle\TwigEngine')
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->eventMock->expects($this->once())
+            ->method('getRequest')
+            ->will($this->returnValue($request));
 
-        $this->listener = new ViewListener($templating);
+        $listener = new ViewListener($this->twigMock);
+        $listener->onControllerResponse($this->eventMock);
+    }
 
-        // mocking away
+    /**
+     * @param string $forController
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject|\Symfony\Component\HttpFoundation\Request
+     */
+    private function getRequestMock($forController)
+    {
         $paramBag = $this->getMockBuilder('\Symfony\Component\HttpFoundation\ParameterBag')
                 ->getMock();
         $paramBag->expects($this->once())
             ->method('get')
-            ->will($this->returnValue('AcmeDemoBundle:Foo:bar'));
+            ->will($this->returnValue($forController));
 
         $request = $this->getMockBuilder('\Symfony\Component\HttpFoundation\Request')
             ->enableArgumentCloning()
             ->getMock();
         $request->attributes = $paramBag;
 
-        $event = $this->getMockBuilder('\Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $event->expects($this->once())
-            ->method('getRequest')
-            ->will($this->returnValue($request));
-
-        $this->listener->onControllerResponse($event);
+        return $request;
     }
 }
